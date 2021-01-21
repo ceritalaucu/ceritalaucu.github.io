20de0d29aa7d2c1f59fd31f22e01e1a3625c20fc
ceritalaucu <notokode@gmail.com>
2021-01-21 12:10:25 +0700

    changes edited files

--- scripts.js
+++ scripts.js
@@ -1,18 +1,12 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+window.onscroll=function(){myFunction()};
+
+var header=document.getElementById("header");
+var sticky=header.offsetTop;
+
 function myFunction(){
-    document.getElementById("Kisah_Show").classList.toggle("show");
-}
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event){
-    if (!event.target.matches(".Nav_Buttons")){
-        var dropdowns = document.getElementsByClassName("Kisah_Content");
-        var i;
-        for (i=0;i<dropdowns.length;i++){
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains("show")){
-                openDropdown.classList.remove("show");
-            }
-        }
+    if(window.pageYOffset>sticky){
+        header.classList.add("sticky");
+    }else{
+        header.classList.remove("sticky");
     }
 }
