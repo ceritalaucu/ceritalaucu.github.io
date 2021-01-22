21af8c600884d7c878242fd3c9fc889ef5f9e0e1
ceritalaucu <notokode@gmail.com>
2021-01-22 22:18:34 +0700

    changes edited files

--- 404.js
+++ 404.js
@@ -4,7 +4,7 @@ var header=document.getElementById("header");
 var sticky=header.offsetTop;
 
 function myFunction(){
-    if(window.pageYOffset>sticky){
+    if(window.pageYOffset>=sticky){
         header.classList.add("sticky");
     }else{
         header.classList.remove("sticky");
