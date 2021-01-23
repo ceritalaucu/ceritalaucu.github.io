9359c6babb6f4d057c93133b7bbb870a813f8e89
ceritalaucu <notokode@gmail.com>
2021-01-23 08:34:23 +0700

    changes edited files

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 window.onscroll=function(){myFunction()};
 
 var header=document.getElementById("header");
-var sticky=header.offsetTop;
 
 function myFunction(){
-    if(window.pageYOffset>sticky){
-        header.classList.add("sticky");
-    }else{
-        header.classList.remove("sticky");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        header.style.top = "0";
+    } else {
+        header.style.top = "-50px";
     }
 }
