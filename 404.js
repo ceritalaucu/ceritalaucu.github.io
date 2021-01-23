3ed31b0085dcad09f97556cff4429fe99210ec31
ceritalaucu <notokode@gmail.com>
2021-01-23 12:35:05 +0700

    changes edited files

--- 404.js
+++ 404.js
@@ -1,12 +1,12 @@
-window.onscroll=function(){myFunction()};
+// window.onscroll=function(){myFunction()};
 
-var header=document.getElementById("header");
-var sticky=header.offsetTop;
+// var header=document.getElementById("header");
+// var sticky=header.offsetTop;
 
-function myFunction(){
-    if(window.pageYOffset>sticky){
-        header.classList.add("sticky");
-    }else{
-        header.classList.remove("sticky");
-    }
-}
+// function myFunction(){
+//     if(window.pageYOffset>sticky){
+//         header.classList.add("sticky");
+//     }else{
+//         header.classList.remove("sticky");
+//     }
+// }
