2aa828cc5d2b5ebf50f7a40bfeb93652f2d44436
ceritalaucu <notokode@gmail.com>
2021-01-27 13:08:28 +0700

    changes edited files

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ $(".checkbtn").click(function(){
     $(this).toggleClass("click");
     $(".header-content").toggleClass("click");
 });
+$(".checkbtn-right").click(function(){
+    $(this).toggleClass("click");
+    $(".header-content").toggleClass("click");
+});
 $(".idnstory-btn").click(function(){
     $("header .idnstory-show").toggleClass("show");
     $("header .first").toggleClass("rotate");
