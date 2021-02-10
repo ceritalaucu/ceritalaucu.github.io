3b091e40dfbed96e9335d0a3991acf2687f96b02
ceritalaucu <notokode@gmail.com>
2021-02-10 11:53:41 +0700

    changes edited files

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ $(".learnbtn").click(function(){
     $(".header-nav .learn-item-show").toggleClass("show");
     $(".header-nav .first").toggleClass("rotate");
 });
+
+function checkTime(i){
+    if(i<10){i="0"+i}; // add zero in front of numbers < 10
+    return i;
+}
+function startTime(){
+    var today=new Date();
+    var hours=today.getHours();
+    var minutes=today.getMinutes();
+    var seconds=today.getSeconds();
+    minutes=checkTime(minutes);
+    seconds=checkTime(seconds);
+    document.getElementById("nowTime").innerHTML=hours+" : "+minutes+" : "+seconds;
+    var t=setTimeout(startTime,500);
+}
