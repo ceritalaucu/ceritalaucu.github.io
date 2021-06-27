21c2f9c4fc4a0bf995dd07d9e7808beab6933714
ceritalaucu <notokode@gmail.com>
2021-06-27 11:44:51 +0700

    changes edited files

--- js/index.js
+++ js/index.js
@@ -1,36 +1,6 @@
 window.onload = () => {
     const preloader = document.querySelector(".loading-page");
     preloader.style.display = "none";
-
-    let today;
-    let hours;
-    let minutes;
-    let seconds;
-    let t;
-
-    function checkTime(i){
-        if(i<10){i="0"+i}; // add zero in front of numbers < 10
-        return i;
-    }
-    // function roadToDays(){
-    //     startTime
-    //     if(hours==00)days=days+1;
-    //     document.getElementById("bh-days").innerHTML="Day "+days;
-    //     t=setTimeout(roadToDays,1000);
-    // }
-    function startTime(){
-        today=new Date();
-        hours=today.getUTCHours()+7;
-        minutes=today.getUTCMinutes();
-        seconds=today.getUTCSeconds();
-        if((hours-24)>=0)hours=hours-24;
-        hours=checkTime(hours);
-        minutes=checkTime(minutes);
-        seconds=checkTime(seconds);
-        document.querySelector(".nowTime").innerHTML=hours+" : "+minutes+" : "+seconds;
-        t=setTimeout(startTime,500);
-    }
-    startTime();
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -65,49 +35,6 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             showLearnItem.style.display = "block";
             rotateLearnBtn.style.transform = "rotate(-180deg)";
-        }
-    });
-
-    const slider = document.querySelector(".slider");
-    let pressed = false;
-    let startX;
-    let scrollLeft;
-    slider.addEventListener("mousedown", (e) => {
-        pressed = true;
-        startX = e.pageX - slider.offsetLeft;
-        scrollLeft = slider.scrollLeft;
-    });
-    slider.addEventListener("mouseleave", () => {
-        pressed = false;
-    });
-    slider.addEventListener("mouseup", () => {
-        pressed = false;
-    });
-    slider.addEventListener("mousemove", (e) => {
-        if(!pressed || window.innerWidth>=1280) return; // stop from scrolling
-        e.preventDefault();
-        const x = e.pageX - slider.offsetLeft;
-        const walk = x - startX;
-        slider.scrollLeft = scrollLeft - walk;
-    });
-    slider.addEventListener("touchstart", (e) => {
-        for (let i=0;i<e.changedTouches.length;i++) {
-            pressed = true;
-            startX = e.changedTouches[i].pageX - slider.offsetLeft;
-            scrollLeft = slider.scrollLeft;    
-        }
-    });
-    slider.addEventListener("touchend", () => {
-        pressed = false;
-    });
-    slider.addEventListener("touchmove", (e) => {
-        for (let i=0;i<e.changedTouches.length;i++) {
-            pressed = true;
-            if(window.innerWidth>=1280) return; // stop from scrolling
-            e.preventDefault();
-            const x = e.changedTouches[i].pageX - slider.offsetLeft;
-            const walk = x - startX;
-            slider.scrollLeft = scrollLeft - walk;
         }
     });
 });
